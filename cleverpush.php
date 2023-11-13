c71e00cff39545a89dde369803b83542c1366e85
gbhrdt <marius.gebhardt@gmail.com>
2023-11-13 17:51:07 +0100

    Release 1.9.3

--- cleverpush.php
+++ cleverpush.php
@@ -4,7 +4,7 @@ Plugin Name: CleverPush
 Plugin URI: https://cleverpush.com
 Description: Send push notifications to your users right through your website. Visit <a href="https://cleverpush.com">CleverPush</a> for more details.
 Author: CleverPush
-Version: 1.9.2
+Version: 1.9.3
 Author URI: https://cleverpush.com
 Text Domain: cleverpush
 Domain Path: /languages
