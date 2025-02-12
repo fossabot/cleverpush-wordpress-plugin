08961319d28dbf21bbca33e13fa3ca7b4662f517
gbhrdt <marius.gebhardt@gmail.com>
2025-02-12 14:22:48 +0100

    Release 1.9.8

--- cleverpush.php
+++ cleverpush.php
@@ -4,7 +4,7 @@ Plugin Name: CleverPush
 Plugin URI: https://cleverpush.com
 Description: Send push notifications to your users right through your website. Visit <a href="https://cleverpush.com">CleverPush</a> for more details.
 Author: CleverPush
-Version: 1.9.7
+Version: 1.9.8
 Author URI: https://cleverpush.com
 Text Domain: cleverpush
 Domain Path: /languages
@@ -19,6 +19,13 @@ require_once 'cleverpush-api.php';
 if (! class_exists('CleverPush') ) :
     class CleverPush
     {
+        /**
+         * Version of the capabilities
+         * 
+         * @var string
+         */
+        private $capabilities_version;
+
         /**
          * Construct the plugin.
          */
--- readme.txt
+++ readme.txt
@@ -4,8 +4,8 @@ Plugin Name: CleverPush
 Plugin URI: https://cleverpush.com
 Tags: push notifications, web push, browser notifications, woocommerce
 Requires at least: 2.7
-Tested up to: 6.4.3
-Stable tag: 1.9.7
+Tested up to: 6.7.2
+Stable tag: 1.9.8
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -33,6 +33,9 @@ Please enter your API keys in the plugin's settings and select a channel. If no 
 If you are using any cache plugin, also be sure, to empty your cache.
 
 == ChangeLog ==
+
+= 1.9.8 =
+* Fixed issue with capabilities_version
 
 = 1.9.7 =
 * Support for tags
